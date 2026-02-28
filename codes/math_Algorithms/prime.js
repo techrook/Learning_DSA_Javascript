7fccb2ca29338fbc55b74fbe24577389dcbe2f36
techrook <mondayitohowo3@gmail.com>
2026-02-28 21:50:36 +0100

    feat: added math algorithm for checking if a number is prime

---
+++ codes/math_Algorithms/prime.js
@@ -0,0 +1,17 @@
+function isPrime(n){
+    let limit = Math.sqrt(n);
+    if (n<2){
+        return false
+    }
+    for(i=2; i<limit; i++){
+        if (n%i === 0){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(2))   // true
+console.log(isPrime(7))   // true
+console.log(isPrime(6))   // false
+console.log(isPrime(1))   // false
